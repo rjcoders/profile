066a2e0255949573e79949f1fc0b0b59e3da0620
Roshan Jha <73661332+rjcoders@users.noreply.github.com>
2024-07-24 13:20:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://github.com/user-attachments/assets/05584809-c36e-46d2-9b50-af1ab2b98a1e)
---
+++ header.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+  
+  <!-- https://www.remove.bg/upload  This is for Remove Images Background-->
+  <!-- https://www.iloveimg.com/ This is for Resize The Images And Crop The Images -->
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- color of address bar in mobile browser -->
+  <meta name="theme-color" content="#2B2B35">
+  <!-- favicon  -->
+  <link rel="shortcut icon" href="img/me.jpg" type="image/x-icon">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+  <!-- font awesome css -->
+  <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+  <!-- swiper css -->
+  <link rel="stylesheet" href="css/plugins/swiper.min.css">
+  <!-- fancybox css -->
+  <link rel="stylesheet" href="css/plugins/fancybox.min.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Website Designing Company in Agra | Web Development Services India | Web Development Company in Agra | App Development Company in Agra</title>
+  <meta name="description" content="Rjcoder Company is an organization which deals in various domains such as web development, etc. We endeavor to be focused on the best operation and creative, innovative ideas around the world and provide high-quality service to the client.">
+</head>
+
+<body>
+
+  <!-- app -->
+  <div class="art-app">
+
+    <!-- mobile top bar -->
+    <div class="art-mobile-top-bar"></div>
+
+    <!-- app wrapper -->
+    <div class="art-app-wrapper">
+
+      <!-- app container end -->
+      <div class="art-app-container">
+
+        <!-- info bar -->
+        <div class="art-info-bar">
+
+          <!-- menu bar frame -->
+          <div class="art-info-bar-frame">
+
+            <!-- info bar header -->
+            <div class="art-info-bar-header">
+              <!-- info bar button -->
+              <a class="art-info-bar-btn" href="#.">
+                <!-- icon -->
+                <i class="fas fa-ellipsis-v"></i>
+              </a>
+              <!-- info bar button end -->
+            </div>
+            <!-- info bar header end -->
+
+            <!-- info bar header -->
+            <div class="art-header">
+              <!-- avatar -->
+              <div class="art-avatar">
+                <a data-fancybox="avatar" href="img/me.jpg" class="art-avatar-curtain">
+                  <img src="img/me.jpg" alt="avatar">
+                  <i class="fas fa-expand"></i>
+                </a>
+                <!-- available -->
+                <div class="art-lamp-light">
+                  <!-- add class 'art-not-available' if not available-->
+                  <div class="art-available-lamp" style="background: #10ff00;"></div>
+                </div>
+              </div>
+              <!-- avatar end -->
+              <!-- name -->
+              <h5 class="art-name mb-10"><a href="index.php">Roshan Jha..!</a></h5>
+              <!-- post -->
+              <div class="art-sm-text">Web Deweloper And <br>Front-end Deweloper, </div>
+            </div>
+            <!-- info bar header end -->
+
+            <!-- scroll frame -->
+            <div id="scrollbar2" class="art-scroll-frame">
+
+              <!-- info bar about -->
+              <div class="art-table p-15-15">
+                <!-- about text -->
+                <ul>
+                  <!-- country -->
+                  <li>
+                    <h6>Residence:</h6><span>Nirman Vihar Delhi</span>
+                  </li>
+                  <!-- city -->
+                  <li>
+                    <h6>City:</h6><span>Delhi</span>
+                  </li>
+                  <!-- age -->
+                  <li>
+                    <h6>Age:</h6><span>22</span>
+                  </li>
+                </ul>
+              </div>
+              <!-- info bar about end -->
+
+              <!-- divider -->
+              <div class="art-ls-divider"></div>
+
+              <!-- language skills -->
+              <div class="art-lang-skills p-30-15">
+
+                <!-- skill -->
+                <div class="art-lang-skills-item">
+                  <div id="circleprog1" class="art-cirkle-progress"></div>
+                  <!-- title -->
+                  <h6>Maithali</h6>
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-lang-skills-item">
+                  <div id="circleprog2" class="art-cirkle-progress"></div>
+                  <!-- title -->
+                  <h6>Hindi</h6>
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-lang-skills-item">
+                  <div id="circleprog3" class="art-cirkle-progress"></div>
+                  <!-- title -->
+                  <h6>English</h6>
+                </div>
+                <!-- skill end -->
+
+              </div>
+              <!-- language skills end -->
+
+              <!-- divider -->
+              <div class="art-ls-divider"></div>
+
+              <!-- hard skills -->
+              <div class="art-hard-skills p-30-15">
+
+                <!-- skill -->
+                <div class="art-hard-skills-item">
+                  <div class="art-skill-heading">
+                    <!-- title -->
+                    <h6>html</h6>
+                  </div>
+                  <!-- progressbar frame -->
+                  <div class="art-line-progress">
+                    <!-- progressbar -->
+                    <div id="lineprog1"></div>
+                  </div>
+                  <!-- progressbar frame end -->
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-hard-skills-item">
+                  <div class="art-skill-heading">
+                    <!-- title -->
+                    <h6>CSS</h6>
+                  </div>
+                  <!-- progressbar frame -->
+                  <div class="art-line-progress">
+                    <!-- progressbar -->
+                    <div id="lineprog2"></div>
+                  </div>
+                  <!-- progressbar frame end -->
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-hard-skills-item">
+                  <div class="art-skill-heading">
+                    <!-- title -->
+                    <h6>Js</h6>
+                  </div>
+                  <!-- progressbar frame -->
+                  <div class="art-line-progress">
+                    <!-- progressbar -->
+                    <div id="lineprog3"></div>
+                  </div>
+                  <!-- progressbar frame end -->
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-hard-skills-item">
+                  <div class="art-skill-heading">
+                    <!-- title -->
+                    <h6>PHP</h6>
+                  </div>
+                  <!-- progressbar frame -->
+                  <div class="art-line-progress">
+                    <!-- progressbar -->
+                    <div id="lineprog4"></div>
+                  </div>
+                  <!-- progressbar frame end -->
+                </div>
+                <!-- skill end -->
+
+                <!-- skill -->
+                <div class="art-hard-skills-item">
+                  <div class="art-skill-heading">
+                    <!-- title -->
+                    <h6>Wordpress</h6>
+                  </div>
+                  <!-- progressbar frame -->
+                  <div class="art-line-progress">
+                    <!-- progressbar -->
+                    <div id="lineprog5"></div>
+                  </div>
+                  <!-- progressbar frame end -->
+                </div>
+                <!-- skill end -->
+
+              </div>
+              <!-- language skills end -->
+
+              <!-- divider -->
+              <div class="art-ls-divider"></div>
+
+              <!-- knowledge list -->
+              <ul class="art-knowledge-list p-15-0">
+                <!-- list item -->
+                <li>Bootstrap, Materialize</li>
+                <!-- list item -->
+                <li>Stylus, Sass, Less</li>
+                <!-- list item -->
+                <li>Gulp, Webpack, Grunt</li>
+                <!-- list item -->
+                <li>GIT knowledge</li>
+              </ul>
+              <!-- knowledge list end -->
+
+              <!-- divider -->
+              <div class="art-ls-divider"></div>
+
+              <!-- links frame -->
+              <div class="art-links-frame p-15-15">
+
+                <!-- download cv button -->
+                <a href="files/cv.txt" class="art-link" download>Download cv <i class="fas fa-download"></i></a>
+
+              </div>
+              <!-- links frame end -->
+
+            </div>
+            <!-- scroll frame end -->
+
+            <!-- sidebar social -->
+            <div class="art-ls-social">
+              <!-- social link -->
+              <a href="#." target="_blank"><i class="fab fa-linkedin-in"></i></a>
+              <!-- social link -->
+              <a href="#." target="_blank"><i class="fab fa-dribbble"></i></a>
+              <!-- social link -->
+              <a href="#." target="_blank"><i class="fab fa-behance"></i></a>
+              <!-- social link -->
+              <a href="#." target="_blank"><i class="fab fa-github"></i></a>
+              <!-- social link -->
+              <a href="#." target="_blank"><i class="fab fa-twitter"></i></a>
+            </div>
+            <!-- sidebar social end -->
+
+          </div>
+          <!-- menu bar frame end -->
+
+        </div>
+        <!-- info bar end -->
